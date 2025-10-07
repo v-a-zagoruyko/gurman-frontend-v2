588c2b843430e172fe2b87fcff4ed7a1f947fd0b
Viktor Zagoruyko <vzagoruyko@rbc.ru>
2025-10-07 11:56:08 +0500

    Fix typo

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,10 +3,14 @@ import defaultImg from "assets/img/default.webp";
 function Card(props) {
   return (
     <div class="flex flex-col rounded-[8px] bg-neutral-50">
-      <img src={props.img || defaultImg} class="rounded-t-[8px]" />
+      <img src={props.image || defaultImg} class="rounded-t-[8px]" />
       <div class="p-[8px] sm:p-[16px]">
-        <p class="mb-[8px] font-oswald text-[17px] sm:text-[20px] leading-none">{props.name}</p>
-        <p class="mb-[4px] text-[15px] sm:text-[17px] text-gray-500 leading-none">{props.description}</p>
+        <p class="mb-[8px] font-oswald text-[17px] sm:text-[20px] leading-none">
+          {props.name}
+        </p>
+        <p class="mb-[4px] text-[15px] sm:text-[17px] text-gray-500 leading-none">
+          {props.description}
+        </p>
         <p class="mt-auto text-[15px] sm:text-[17px] text-gray-500 text-right font-medium leading-none">
           {props.weight}, {props.price} ₽
         </p>
